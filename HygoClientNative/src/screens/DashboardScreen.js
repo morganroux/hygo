3f0131b4a7cc19c7fdba7a72c875e94499348909
Morgan <morganitos@hotmail.com>
2019-10-19 16:09:41 +0200

    enhance dashboard : add progress bars, and colors

--- HygoClientNative/src/screens/DashboardScreen.js
+++ HygoClientNative/src/screens/DashboardScreen.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import {View, StyleSheet, Text, Button} from 'react-native';
+import {View } from 'react-native';
+import { Text, Button} from 'react-native-elements';
 import { connect } from 'react-redux';
+import { Paper } from '@material-ui/core';
+
+import ProgressBar from 'react-native-progress/Bar';
 
 const DashboardScreen = ( {navigation}) =>  (
-    <React.Fragment>
-        <Text style = {{fontSize: 48}}>Dashboard</Text>
-    </React.Fragment>
+    
+    <View>
+    <Text h1>Dashboard</Text>
+    <Text h3 style={{backgroundColor: '#EBF6EC'}}>Bonnes conditions</Text>
+    <Text h5>Température</Text>
+    <ProgressBar
+        progress={0.3}
+        color={'green'}
+        size={50}
+    />
+    <Text h5>Hygrométrie</Text>
+    <ProgressBar
+        progress={0.7}
+        color={'blue'}
+        size={50}
+    />
+    </View>
 );
 
 const mapStateToProps = ({token, userName}) => ({
